69e744a67209077faf56cb780a5c2c34fc730fdc
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-03-04 10:35:45 +0530

    added Navbar

--- src/components/shared/Navbar/index.js
+++ src/components/shared/Navbar/index.js
@@ -1,9 +1,31 @@
 import React from 'react'
+import logo from '../../../assets/logo.png'
 import './style.css'
 
 function Navbar() {
     return (
-        <div>Navbar</div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ justifyContent: 'flex-end' }}>
+                    <ul className="navbar-nav mb-2 me-5 mb-lg-0">
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="/">
+                                <span className='posua'>P<img src={logo} alt='logo' />SUA</span>
+                            </a>
+                        </li>
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="/">Event</a>
+                        </li>
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="/">Gallery</a>
+                        </li>
+                        <li className="nav-item mx-4">
+                            <a className="nav-link active" href="/">Team</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
