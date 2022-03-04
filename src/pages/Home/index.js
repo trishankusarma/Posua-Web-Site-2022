4042e1499c2cf8db20f820a57aa4e14f99708417
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-03-05 01:15:45 +0530

    added timer

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Prev from '../../components/Home/Prev'
+import Sponsers from '../../components/Home/Sponsers'
+import Timer from '../../components/Home/Timer'
 import './style.css'
 
 const HomePage = () => {
   return (
     <>
+      <Sponsers />
       <Prev />
+      <Timer />
     </>
   )
 }
