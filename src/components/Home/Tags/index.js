6c0da3001800610bc0d971e0fbedb2a010aaf0aa
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-03-06 18:46:06 +0530

    home page

---
+++ src/components/Home/Tags/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+
+function Tags() {
+    return (
+        <div className='tags py-5'>
+            <div className='content'>
+                <h3 className='py-3'>#tags</h3>
+                <p>
+                    #চেনেহৰ_বতৰা_লৈ_পছোৱা_আহিছে_ঐ<br />
+                    #দশকৰ_পদূলিত_পছোৱাৰ_ভুমুকি<br /><br />
+
+                    #Posua_reviving_exuberance!<br />
+                    #espying_10_years_of_ecstasy
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Tags
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import About from '../../components/Home/About'
-import Accordion from '../../components/Home/Accordion'
-import Cover from '../../components/Home/Cover'
-import Prev from '../../components/Home/Prev'
-import Sponsers from '../../components/Home/Sponsers'
-import Timer from '../../components/Home/Timer'
+import Invitation from '../../components/Home/Invitation'
+import Tags from '../../components/Home/Tags'
+import Cover from '../../components/shared/Cover'
+import Sponsors from '../../components/shared/Sponsors'
+import Timer from '../../components/shared/Timer'
 import './style.css'
 
 const HomePage = () => {
@@ -12,9 +12,9 @@ const HomePage = () => {
     <>
       <Cover />
       <About />
-      <Sponsers />
-      <Prev />
-      <Accordion />
+      <Invitation />
+      <Sponsors />
+      <Tags />
       <Timer />
     </>
   )
