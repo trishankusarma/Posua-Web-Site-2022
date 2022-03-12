761733b1a7bf828a7d74682074807cf5d5291c08
Trishanku Sarma <trishankusarma165@gmail.com>
2022-03-12 22:56:10 +0530

    added names and roles and mails in teams page

--- src/data/team.js
+++ src/data/team.js
@@ -1,43 +1,57 @@
 import c1 from '../assets/c1.png'
 import c2 from '../assets/c2.png'
+import c3 from '../assets/c3.png'
+import c4 from '../assets/c4.png'
+import c5 from '../assets/c5.png'
+import c6 from '../assets/c6.png'
+import c7 from '../assets/c7.png'
+import c8 from '../assets/c8.png'
+import c9 from '../assets/c9.png'
+import c10 from '../assets/c10.png'
+import c11 from '../assets/c11.png'
+import c12 from '../assets/c12.png'
+import c13 from '../assets/c13.png'
+import c14 from '../assets/c14.png'
+import c15 from '../assets/c15.png'
+import c16 from '../assets/c16.png'
 
 const teams = [
     {
-        name : "name" , 
-        role : "role" , 
-        phoneNo : "phoneNo" , 
-        email : "email" ,
+        name : "Prof. Sivaji Bandyopadhyay" , 
+        role : "Chief Patron" , 
+        phoneNo : "03842-242273" , 
+        email : "director@nits.ac.in" ,
         img : c1
     },
 
     {
-        name : "name" , 
-        role : "role" , 
+        name : "Prof. Mokaddes Ali Ahmed" , 
+        role : "President" , 
         phoneNo : "phoneNo" , 
-        email : "email" ,
+        email : "ali.mokaddes@gmail.com" ,
         img : c2
     } ,
 
     {
-        name : "name" , 
-        role : "role" , 
+        name : "Dr. Ripon Patgiri" , 
+        role : "Vice President" , 
         phoneNo : "phoneNo" , 
-        email : "email" ,
+        email : "ripon.patgiri@gmail.com" ,
+        img : c3
+    } ,
+
+    {
+        name : "Dr. Malaya Dutta Borah" , 
+        role : "Vice President" , 
+        phoneNo : "phoneNo" , 
+        email : "malayaduttaborah@gmail.com" ,
         img : c2
     } ,
 
     {
-        name : "name" , 
-        role : "role" , 
-        phoneNo : "phoneNo" , 
-        email : "email" ,
-        img : c2
-    } ,
-
-    {
-        name : "name" , 
-        role : "role" , 
-        phoneNo : "phoneNo" , 
+        name : "Dr. Pranjit Barman" , 
+        role : "Mentor" , 
+        phoneNo : "+919435374128" , 
         email : "email" ,
         img : c2
     } ,
